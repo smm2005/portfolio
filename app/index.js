2a5d63a6669499bb729510181852ece868de3b24
Suhail Moeen <suhailmmoeen@gmail.com>
2025-05-05 14:43:24 -0400

    Initializing portfolio website

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+app.use(express.static('static'));
+
+app.listen(port, hostname, ()=>{
+    console.log(`Listening to app on http://${hostname}:${port}/`);
+});
