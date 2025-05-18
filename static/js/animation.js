33085e9d26e983831edb938f7d8ed741a61cb377
Suhail Moeen <suhailmmoeen@gmail.com>
2025-05-18 19:16:34 -0400

    Feat: CSS animations

---
+++ static/js/animation.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let atag = document.getElementById("navbar").getElementsByClassName("hov");
+    Array.from(atag).forEach((hov) => {
+            hov.addEventListener("mouseover", ()=>{
+                hov.classList.add("hovered");
+            });
+
+            hov.addEventListener("mouseout", ()=>{
+                hov.classList.add("unhovered");
+                hov.classList.remove("hovered");
+                setTimeout(()=>{ hov.addEventListener("animationend", animationEnd()); }, 1000);
+            });
+
+            function animationEnd(){
+                hov.classList.remove("unhovered", "hovered");
+                hov.removeEventListener("animationend", animationEnd);
+            }; 
+    });
+});
