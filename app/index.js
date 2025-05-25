0a886f5797c7d19b5da4362190f6433781f8573f
Suhail Moeen <suhailmmoeen@gmail.com>
2025-05-25 17:09:27 -0400

    Feat: Migration from single-page to multi-page content

--- app/index.js
+++ app/index.js
@@ -6,12 +6,16 @@ const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
 const path = require('node:path');
 
-router.use("/static", express.static("static"));
+router.use(express.static("static"));
 
-app.use('/app/', router);
+app.use('/', router);
 module.exports.handler = serverless(app);
 
+app.get('/', (req, res) => {
+
+});
+
 app.listen(port, hostname, ()=>{
-    console.log(`Listening to app on http://${hostname}:${port}/app/static`);
+    console.log(`Listening to app on http://${hostname}:${port}/app/`);
 });
 
